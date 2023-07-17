78f6b22911cd7780d055fc03676a8d9e48d4891e
Landon Fowler <32523792+landonkfowler@users.noreply.github.com>
2023-07-17 14:35:48 -0400

    Adding arithmetic_operations.py

---
+++ arithmetic_operations.py
@@ -0,0 +1,13 @@
+# Variables and Data Types
+apples = 1  # An integer variable
+juice_glasses = 2.5  # A float variable
+
+# Arithmetic Operations
+total_apples = apples + 2  # Adding 2 to the apples variable
+half_juice = juice_glasses / 2  # Dividing juice_glasses by 2
+combined_amount = apples + juice_glasses  # Adding apples and juice_glasses together
+
+# Printing the results
+print("Total apples:", total_apples)
+print("Half juice:", half_juice)
+print("Combined amount:", combined_amount)
